a901ea3b809e99b358938baf4c8a6b9982b8062a
Zacharie Boisnard <mecazack@gmail.com>
2024-02-15 22:08:58 +0100

    init

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import * as path from 'path';
+import * as express from 'express';
+import { Express } from 'express';
+
+const app: Express = express();
+
+app.use(express.json());
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,115 @@
+import { Express } from 'express';
+import * as dotenv from 'dotenv';
+import * as http from 'http';
+import * as https from 'https';
+import * as fs from 'fs';
+import app from '@src/app';
+
+// Initialise les variables d'environnement
+dotenv.config({ path: './.env' });
+
+export default class Server {
+    private port: any;
+    private app: Express = app;
+    private options: ServerOptionsCert | null;
+
+    private server:
+        | https.Server<typeof http.IncomingMessage, typeof http.ServerResponse>
+        | http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>;
+
+    constructor() {
+        this.port = this.normalizePort(process.env.PORT || '3000');
+
+        this.options = this.checkHttpsConfig
+            ? {
+                  key: fs.readFileSync('selfsigned.key'),
+                  cert: fs.readFileSync('selfsigned.crt'),
+              }
+            : null;
+
+        this.setServer();
+    }
+
+    private normalizePort(val: any): any {
+        const port = parseInt(val, 10);
+        if (isNaN(port)) {
+            return val;
+        }
+        if (port >= 0) {
+            return port;
+        }
+        return false;
+    }
+
+    private errorHandler(error: any) {
+        console.log(error);
+        if (error.syscall !== 'listen') {
+            throw error;
+        }
+        const address = this.server.address();
+        const bind =
+            typeof address === 'string'
+                ? 'pipe ' + address
+                : 'port: ' + this.port;
+        switch (error.code) {
+            case 'EACCES':
+                console.error(bind + ' requires elevated privileges.');
+                process.exit(1);
+            //break;
+            case 'EADDRINUSE':
+                console.error(bind + ' is already in use.');
+                process.exit(1);
+            //break;
+            default:
+                throw error;
+        }
+    }
+
+    private setAppExpress() {
+        this.app.set('port', this.port);
+    }
+
+    private setServer() {
+        this.setAppExpress();
+
+        this.server = this.checkHttpsConfig
+            ? https.createServer(this.options, this.app)
+            : http.createServer(this.app);
+
+        this.server.on('error', this.errorHandler);
+
+        this.server.on('listening', () => {
+            const address = this.server.address();
+            const bind =
+                typeof address === 'string'
+                    ? 'pipe ' + address
+                    : 'port ' + this.port;
+            console.log('Listening on ' + bind);
+        });
+    }
+
+    public listen() {
+        this.server.listen(this.port);
+    }
+
+    /**
+     * Traite la configuration https et vérifie sa conformité si elle vient d'une variable d'environnement
+     * @returns
+     */
+    private checkHttpsConfig(): boolean {
+        const value = process.env.PROTOCOL || 'HTTP';
+        switch (value.toUpperCase()) {
+            case 'HTTPS':
+                return true;
+            case 'HTTP':
+                return false;
+            default:
+                return false;
+        }
+    }
+}
+
+interface ServerOptionsCert {
+    key: Buffer;
+    cert: Buffer;
+}
